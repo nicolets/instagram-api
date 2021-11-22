4faf4342d1c5eb1ca4ab1f176b9ec99758ccf806
Nicole <nicole30401@gmail.com>
2021-11-22 19:47:20 +0200

    port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 const routes = require('./config/routes')
 const env = require('./config/env/index')
 
